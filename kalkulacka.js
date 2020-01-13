ca5f291a11303de3f4cb2a3be012071690317bf6
petrbinar <bin.petr@gmail.com>
2020-01-13 16:08:09 +0100

    auto-kalkulacka spotreby nafty

---
+++ kalkulacka.js
@@ -0,0 +1,24 @@
+let cenaNafty = 33;
+let spotrebaNa100 = 6.5;
+let vzdalenost = 145;
+let pocetlidi = 4;
+
+const nadpis = "              kalkulacka cestovnich nakladu               ";
+console.log(nadpis);
+
+let textCenaNafty = `Cena nafty ${cenaNafty} Kc/l`;
+console.log(textCenaNafty);
+let textSpotrebaNa100 = `Spotreba nafty  ${spotrebaNa100}l/100km`;
+console.log(textSpotrebaNa100);
+
+let textUjetaVzdalenost = `Ujeta vzdalenost = ${vzdalenost} Km`;
+console.log(textUjetaVzdalenost);
+
+let cenaZaKm = (cenaNafty * spotrebaNa100) / 100;
+console.log(`Cena za 1Km = ${cenaZaKm} Kc`);
+
+let textZaVzdalenost = cenaZaKm * vzdalenost;
+console.log(`Celkova cena za vzdalenost ${textZaVzdalenost} Kc`);
+
+let cenaNaOsobu = Math.ceil(textZaVzdalenost / pocetlidi);
+console.log(`Zaokrouhlena cena na osobu je ${cenaNaOsobu} Kc`);
