6a25a0d09e3cd664a0d453a7353a81e94557977a
petrbinar <bin.petr@gmail.com>
2020-01-14 07:59:31 +0100

    vese s funkcema - pracovni

--- kalkulacka.js
+++ kalkulacka.js
@@ -1,16 +1,11 @@
-// Vstupni hodnoty
-const cenaNafty = 33;
-const spotrebaNa100 = 6.5;
-const vzdalenost = 145;
-const pocetLidi = 4;
+function spocitejSpotrebu(cenaNafty, spotrebaNa100, vzdalenost, pocetLidi) {
+  // Vypocet
+  const cenaZaKm = (cenaNafty * spotrebaNa100) / 100;
+  const textCenaZaVzdalenost = cenaZaKm * vzdalenost;
+  const cenaNaOsobu = Math.ceil(textCenaZaVzdalenost / pocetLidi);
 
-// Vypocet
-const cenaZaKm = (cenaNafty * spotrebaNa100) / 100;
-const textCenaZaVzdalenost = cenaZaKm * vzdalenost;
-const cenaNaOsobu = Math.ceil(textCenaZaVzdalenost / pocetLidi);
-
-// Vypis do konzole
-console.log(`               Kalkulacka cestovnich nakladu 
+  // Vypis do konzole
+  console.log(`               Kalkulacka cestovnich nakladu 
 
     Cena nafty                      =   ${cenaNafty} Kc/l
     Spotreba nafty                  =   ${spotrebaNa100} l/100Km
@@ -19,3 +14,14 @@ console.log(`               Kalkulacka cestovnich nakladu
     Cena za 1Km                     =   ${cenaZaKm} Kc
     Celkova cena za vzdalenost      =   ${textCenaZaVzdalenost} Kc
     Zaokrouhlena cena na osobu je   =   ${cenaNaOsobu} Kc`);
+}
+
+//spocitejSpotrebu(30, 6, 500, 3);
+
+function nasobeniDvema(cisloKNasobeni) {
+  return cisloKNasobeni * 2;
+}
+
+const vysledek = nasobeniDvema(666);
+
+console.log(nasobeniDvema(2));
