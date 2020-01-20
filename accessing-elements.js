0b7b382f96556cdbb73420ef5f50af518ca013a3
petrbinar <bin.petr@gmail.com>
2020-01-20 13:20:55 +0100

    test array

---
+++ accessing-elements.js
@@ -0,0 +1,9 @@
+console.clear();
+
+const zkouska = `             tohle je muj zkusebni text       `;
+
+console.log(zkouska);
+
+const myArray = ["pavel", "petr", "jirik", "ali"];
+
+console.log(myArray);
