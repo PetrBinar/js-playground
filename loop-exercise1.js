cf47bc4d0061e236b38683da92c59907cc991973
petrbinar <bin.petr@gmail.com>
2020-01-22 16:34:47 +0100

    pricitani,

---
+++ loop-exercise1.js
@@ -0,0 +1,16 @@
+console.clear();
+// vypis rady cisel od 0 po 3,s pricitanim (++) po +1
+for (let promena = 0; promena < 4; promena++) {
+  console.log(promena);
+}
+
+// The loop below loops from 0 to 3. Edit it to loop backwards from 3 to 0
+for (let counter = 3; counter >= 0; counter--) {
+  console.log(counter);
+}
+
+const animals = ["Grizzly Bear", "Sloth", "Sea Lion"];
+
+for (let index = 0; index < animals.length; index++) {
+  console.log("animals " + animals[index]);
+}
