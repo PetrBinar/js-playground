d04525924f3fc0fb041b39d1f0c331077bc7ce02
petrbinar <bin.petr@gmail.com>
2020-01-22 12:50:14 +0100

    mutace Array - zlomeni reference

--- example-02.js
+++ example-02.js
@@ -1,6 +1,10 @@
 console.clear();
 
 const startovniHodnota = ["arrays", "can", "be", "mutated"];
+//globalne definovana promena
+//nasledna funkce by menila globalne definovanou promenou,
+//tak se promena vystrinkuje aby se stratily vsechny reference a
+//nasledne se ten strink prevede - sparsuje zpet na novy obekt
 
 function changeArr(predanaHodnota) {
   //   console.log(startovniHodnota);
